9abd83c5a0acef03e1b77317b29eb97286e34828
Serhat MBPro <dikme-ucusan-0z@icloud.com>
2024-05-06 15:48:07 +0300

    Initial Commit

---
+++ AdvancedL/AdvancedLApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdvancedLApp.swift
+//  AdvancedL
+//
+//  Created by serhatakyol on 6.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct AdvancedLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
